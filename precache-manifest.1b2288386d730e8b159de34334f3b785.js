170dc870bfe13a3cfddf2929bc124c50b9122907
Rangulf <leperman@gmail.com>
2020-10-19 10:38:33 +0800

    Updates

---
+++ precache-manifest.1b2288386d730e8b159de34334f3b785.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bd6815d8b5d46adf23be98b1314105",
+    "url": "/monsters-rolodex-2/index.html"
+  },
+  {
+    "revision": "87292c8cb9cdcce3447e",
+    "url": "/monsters-rolodex-2/static/css/main.ee6b8a4b.chunk.css"
+  },
+  {
+    "revision": "fdf293ba6c1733d9c89a",
+    "url": "/monsters-rolodex-2/static/js/2.9bce343f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex-2/static/js/2.9bce343f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87292c8cb9cdcce3447e",
+    "url": "/monsters-rolodex-2/static/js/main.bff1a5f4.chunk.js"
+  },
+  {
+    "revision": "cf92c333329a4a79ca4e",
+    "url": "/monsters-rolodex-2/static/js/runtime-main.f4b37482.js"
+  }
+]);
